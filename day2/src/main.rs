0049a124fda207945ee2bdb3f49bab5a6b5909c8
Cat Cai <catherinetcai@gmail.com>
2020-12-02 22:53:26 +0000

    Done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,6 +3,9 @@ use std::fs::File;
 use std::io::prelude::*;
 use regex::Regex;
 
+// Definitely ways to improve this :vomit: code.
+// Function signatures match, so literally could just chain the validations and spit out the
+// outputs
 fn main() {
     let file = File::open("input.txt").unwrap();
     let buf = BufReader::new(file);
@@ -13,7 +16,7 @@ fn main() {
     let re = Regex::new(r"(?P<min>\d+)\-(?P<max>\d+)\s*(?P<letter>[a-zA-Z]{1}):\s*(?P<password>[a-zA-Z]+)").unwrap();
     for line in buf.lines() {
         let n = line.unwrap();
-        if valid_password(&re, n.as_str()) {
+        if valid_password_2(&re, n.as_str()) {
             counter += 1;
         } else {
             println!("Didn't find a valid password: {}", n);
@@ -22,7 +25,8 @@ fn main() {
     println!("Final count of valid passwords: {}", counter);
 }
 
-fn valid_password(re: &Regex, input: &str) -> bool {
+// Validates whether or not the # of occurrences with the letter
+fn valid_password_1(re: &Regex, input: &str) -> bool {
     // let matched = re.captures("1-8 n: dpwpmhknmnlglhjtrbpx");
     match re.captures(input) {
         Some(matched) => {
@@ -43,6 +47,42 @@ fn valid_password(re: &Regex, input: &str) -> bool {
             if matches >= min && matches <= max {
                 return true;
             }
+            return false;
+        },
+        None => {
+            println!("Issue with input: {}", input);
+            return false;
+        }
+    }
+}
+
+// Validates positioning
+fn valid_password_2(re: &Regex, input: &str) -> bool {
+    match re.captures(input) {
+        Some(matched) => {
+            let min: usize = matched.name("min").unwrap().as_str().parse().unwrap();
+            let max: usize = matched.name("max").unwrap().as_str().parse().unwrap();
+            let letter = matched.name("letter").unwrap().as_str().to_string();
+            let password = matched.name("password").unwrap().as_str();
+
+            let mut matches = 0;
+
+
+            for (i, c) in password.chars().enumerate() {
+                println!("Parsed string: min {}, max {}, letter {}, password {}, count: {}", min, max, letter, password, matches);
+                let idx = i + 1;
+                if idx == min || idx == max {
+                    if c.to_string() == letter.to_string() {
+                        println!("Testing char: {}, letter: {}", c.to_string(), letter.to_string());
+                        matches += 1
+                    }
+                }
+            }
+
+            if matches == 1 {
+                return true
+            }
+
             return false;
         },
         None => {
