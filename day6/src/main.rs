0a86eb18eb2e2fbc9646a71e19cf4b3e74ae99a6
Cat Cai <catherinetcai@gmail.com>
2020-12-06 23:01:33 +0000

    Day 6 complete

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,14 +1,16 @@
 use std::io::{BufReader};
 use std::fs::File;
 use std::io::prelude::*;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let file = File::open("input.txt").unwrap();
     let buf = BufReader::new(file);
 
     let mut datas = vec![HashSet::new()];
+    let mut datas_two: Vec<HashMap<String, u32>> = vec![HashMap::new()];
 
+    // Solution 1
     for line in buf.lines() {
         let unwrapped = line.unwrap();
         println!("Line: {}", unwrapped);
@@ -17,23 +19,42 @@ fn main() {
             // We know that this is the end of a passport builder
             println!("This is an empty line!");
             datas.push(HashSet::new());
+            datas_two.push(HashMap::new());
         } else {
+            let last = datas_two.last_mut().unwrap();
+            *last.entry("count".to_string()).or_insert(0) += 1;
             for k in unwrapped.split("") {
                 if k.is_empty() {
                     continue
                 }
                 datas.last_mut().unwrap().insert(k.to_string());
+                *last.entry(k.to_string()).or_insert(0) += 1;
             }
         }
     }
 
-    let mut sum = 0;
+    let mut sum_one = 0;
 
     for data in datas {
         let length = data.len();
-        println!("Data: {:?}, Length: {}", data, length);
-        sum += length;
+        sum_one += length;
     }
 
-    println!("Sum: {}", sum);
+    println!("Sum: {}", sum_one);
+
+    let mut sum_two = 0;
+
+    for data in datas_two {
+        println!("Data two: {:?}", data);
+        for (k, v) in data.iter() {
+            if k == "count" {
+                continue
+            }
+            if *v == data["count"] {
+                sum_two += 1;
+            }
+        }
+    }
+
+    println!("Sum two: {}", sum_two);
 }
